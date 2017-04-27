df6959c8f74b357120e4f747c8b91ed5108386be
Gaël <gael@hungryup.co>
2017-04-28 00:11:15 +0200

    clean template data

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" />
     <h1 class="title">
       {{ error.statusCode }}
     </h1>
@@ -17,21 +16,3 @@ export default {
   props: ['error'],
 };
 </script>
-
-<style scoped>
-.title
-{
-  margin-top: 15px;
-  font-size: 5em;
-}
-.info
-{
-  font-weight: 300;
-  color: #9aabb1;
-  margin: 0;
-}
-.button
-{
-  margin-top: 50px;
-}
-</style>
