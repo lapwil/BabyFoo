fcebfc8d853650614dba4bdf471cea0d201f74f5
Gaël <gael@hungryup.co>
2017-05-05 22:29:21 +0200

    Project layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+    <div>
+        <nav>
+            <nuxt-link to="/" class="brand"><span>Babyfoo</span></nuxt-link>
+            <div class="menu">
+                <nuxt-link to="/scores" class="pseudo button">Scores</nuxt-link>
+                <nuxt-link to="/users" class="pseudo button">Users</nuxt-link>
+            </div>
+        </nav>
+        <nuxt class="body"/>
+    </div>
 </template>
-
-<script>
-export default {};
-</script>
