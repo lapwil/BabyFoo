4771454964085a0fd36be5c8f5e5edb8215dff7f
Gaël <gael@hungryup.co>
2017-05-05 22:28:34 +0200

    less strict

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,15 @@ module.exports = {
     // add your custom rules here
     rules: {
         'no-shadow': 0,
+        'no-underscore-dangle': 0,
         'import/no-unresolved': 0,
+        'import/extensions': 0,
         'no-param-reassign': 0,
         'max-len': ['error', 120],
         'no-bitwise': 0,
-        'import/no-extraneous-dependencies': [2, {"peerDependencies": true}]
+        'import/no-extraneous-dependencies': [2, {"peerDependencies": true}],
+        'no-restricted-syntax': 0
+
     },
     globals: {
         document: true,
