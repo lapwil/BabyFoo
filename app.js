c04d4f71ff90e05a7ea9f184222dfa4846858dc2
Gaël <gael@hungryup.co>
2017-05-05 22:28:52 +0200

    setup generic api crud

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const koaBody = require('koa-bodyparser');
+const mongoose = require('koa-mongoose');
+const json = require('koa-json');
 const Nuxt = require('nuxt');
+
 const config = require('./nuxt.config.js');
+const genericCRUD = require('./server/generic.js');
 
 const app = new Koa();
+const router = new KoaRouter();
 config.dev = !(app.env === 'production');
 
 const nuxt = new Nuxt(config);
@@ -15,6 +22,29 @@ if (config.dev) {
     process.exit(1);
   });
 }
+
+app.use(mongoose({
+  username: '',
+  password: '',
+  host: '127.0.0.1',
+  port: 27017,
+  database: 'babyfoo',
+  schemas: `${__dirname}/schemas`,
+  db: {
+    native_parser: true,
+  },
+  server: {
+    poolSize: 5,
+  },
+}));
+
+app.use(json());
+app.use(koaBody());
+router.use('/api/users', genericCRUD('User'));
+router.use('/api/scores', genericCRUD('Score'));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.use(async (ctx) => {
   ctx.status = 200; // koa defaults to 404 when it sees that status is unset
---
+++ server/generic.js
@@ -0,0 +1,18 @@
+const KoaRouter = require('koa-router');
+
+module.exports = function generic(modelName) {
+  const router = new KoaRouter();
+  router.post('/', async (ctx) => {
+    const { model, request } = ctx;
+    const User = model(modelName);
+    const user = new User(request.body);
+    await user.save();
+    ctx.body = user;
+  });
+  router.get('/', async (ctx) => {
+    const { model } = ctx;
+    const User = model(modelName);
+    ctx.body = await User.find();
+  });
+  return router.routes();
+};
